03b6261f03ac890a9181f2c5f8b9524af440a661
Meitar Moscovitz <meitarm@gmail.com>
2017-03-12 07:19:40 -0400

    Obsessively escape output, complete internationalization for HTML.
    This commit makes several minor changes that perform the following:
    
    * Wraps all interface text with i18n functions.
    * Include a Text Domain for the WordPress header to prepare for l10n.
    * Replace the constant used as the text domain with a string literal.
      * This is a WordPres plugin developer's best practice. See:
        https://developer.wordpress.org/plugins/internationalization/how-to-internationalize-your-plugin/
    * Wrap all printed strings with appropriate output escaping functions.
    * Explicit denote the plugin's GPL-3.0 license in a PHP DocBlock.

--- nrkbetaquiz.php
+++ nrkbetaquiz.php
@@ -1,11 +1,21 @@
 <?php
-/*
-Plugin Name: NRKBeta Know2Comment
-Version: 1.0.0
-Plugin URI: https://nrkbeta.no/
-Author: Henrik Lied and Eirik Backer, Norwegian Broadcasting Corporation
-Description: Require the user to answer a quiz to be able to post comments.
-*/
+/**
+ * Know2Comment plugin for WordPress.
+ *
+ * WordPress plugin header information:
+ *
+ * * Plugin Name: NRKBeta Know2Comment
+ * * Plugin URI: https://nrkbeta.no/
+ * * Version: 1.0.0
+ * * Description: Require the user to answer a quiz to be able to post comments.
+ * * Author: Henrik Lied and Eirik Backer, Norwegian Broadcasting Corporation
+ * * Text Domain: nrkbetaquiz
+ * * Domain Path: /languages
+ *
+ * @link https://developer.wordpress.org/plugins/the-basics/header-requirements/
+ *
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
 
 define('NRKBCQ', 'nrkbetaquiz');
 define('NRKBCQ_NONCE', NRKBCQ . '-nonce');
@@ -17,16 +27,16 @@ add_action('wp_enqueue_scripts', function(){
 
 add_action('comment_form_before', 'nrkbetaquiz_form');
 function nrkbetaquiz_form(){ ?>
-  <div class="<?php echo NRKBCQ; ?>"
-    data-<?php echo NRKBCQ; ?>="<?php echo esc_attr(urlencode(json_encode(get_post_meta(get_the_ID(), NRKBCQ)))); ?>"
-    data-<?php echo NRKBCQ; ?>-error="<?php echo esc_attr(__('You have not answered the quiz correctly. Try again.', NRKBCQ)); ?>">
-    <h2>Would you like to comment? Please answer some quiz questions from the story.</h2>
-    <p>
+  <div class="<?php esc_attr_e(NRKBCQ); ?>"
+    data-<?php esc_attr_e(NRKBCQ); ?>="<?php echo esc_attr(urlencode(json_encode(get_post_meta(get_the_ID(), 'nrkbetaquiz')))); ?>"
+    data-<?php esc_attr_e(NRKBCQ); ?>-error="<?php esc_attr_e('You have not answered the quiz correctly. Try again.', 'nrkbetaquiz'); ?>">
+    <h2><?php esc_html_e('Would you like to comment? Please answer some quiz questions from the story.', 'nrkbetaquiz');?></h2>
+    <p><?php esc_html_e("
       We care about our comments.
       That's why we want to make sure that everyone who comments have actually read the story.
       Answer a couple of questions from the story to unlock the comment form.
-    </p>
-    <noscript>Please <a href="http://enable-javascript.com/" target="_blank" style="text-decoration:underline">enable javascript</a> to comment</noscript>
+    ", 'nrkbetaquiz');?></p>
+    <noscript><?php _e(sprintf(esc_html('Please %1$senable javascript%2$s to comment'), '<a href="http://enable-javascript.com/" target="_blank" style="text-decoration:underline">', '</a>'), 'nrkbetaquiz');?></noscript>
   </div>
 <?php }
 
@@ -37,31 +47,31 @@ function nrkbetaquiz_add(){
 
 function nrkbetaquiz_edit($post){
   $questions = array_pad(get_post_meta($post->ID, NRKBCQ), 1, array());
-  $addmore = esc_html(__('Add question +', NRKBCQ));
-  $correct = esc_html(__('Correct', NRKBCQ));
-  $answer = esc_attr(__('Answer', NRKBCQ));
+  $addmore = esc_html(__('Add question +', 'nrkbetaquiz'));
+  $correct = esc_html(__('Correct', 'nrkbetaquiz'));
+  $answer = esc_attr(__('Answer', 'nrkbetaquiz'));
 
   foreach($questions as $index => $question){
-    $title = __('Question', NRKBCQ) . ' ' . ($index + 1);
+    $title = __('Question', 'nrkbetaquiz') . ' ' . ($index + 1);
     $text = esc_attr(empty($question['text'])? '' : $question['text']);
     $name = NRKBCQ . '[' . $index . ']';
 
     echo '<div style="margin-bottom:1em;padding-bottom:1em;border-bottom:1px solid #eee">';
-    echo '<label><strong>' . $title . ':</strong><br><input type="text" name="' . $name . '[text]" value="' . $text . '"></label>';
+    echo '<label><strong>' . esc_html($title) . ':</strong><br><input type="text" name="' . esc_attr($name) . '[text]" value="' . esc_attr($text) . '"></label>';
     for($i = 0; $i<3; $i++){
       $check = checked($i, isset($question['correct'])? intval($question['correct']) : 0, false);
       $value = isset($question['answer'][$i])? esc_attr($question['answer'][$i]) : '';
 
-      echo '<br><input type="text" name="' . $name . '[answer][' . $i . ']" placeholder="' . $answer . '" value="' . $value . '">';
-      echo '<label><input type="radio" name="' . $name . '[correct]" value="' . $i . '"' . $check . '> ' . $correct . '</label>';
+      echo '<br><input type="text" name="' . esc_attr($name) . '[answer][' . esc_attr($i) . ']" placeholder="' . esc_attr($answer) . '" value="' . esc_attr($value) . '">';
+      echo '<label><input type="radio" name="' . esc_attr($name) . '[correct]" value="' . esc_attr($i) . '"' .$check . '> ' . esc_html($correct) . '</label>';
     }
     echo '</div>';
   }
-  echo '<button class="button" type="button" data-' . NRKBCQ . '>' . $addmore . '</button>';
+  echo '<button class="button" type="button" data-' . esc_attr(NRKBCQ) . '>' . esc_html($addmore) . '</button>';
 
   ?><script>
     document.addEventListener('click', function(event){
-      if(event.target.hasAttribute('data-<?php echo NRKBCQ; ?>')){
+      if(event.target.hasAttribute('data-<?php echo esc_js(esc_attr(NRKBCQ)); ?>')){
         var button = event.target;
         var index = [].indexOf.call(button.parentNode.children, button);
         var clone = button.previousElementSibling.cloneNode(true);
